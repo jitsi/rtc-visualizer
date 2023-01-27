a7a7b8c3487293fed0cdfac873491ac3c78065f0
George Politis <gp@jitsi.org>
2023-01-27 11:45:46 +0000

    Allows anonymous access to the stats display page.

--- src/server/auth.mjs
+++ src/server/auth.mjs
@@ -3,17 +3,17 @@ import basicAuth from 'express-basic-auth'
 
 const { USERS_FILE } = process.env
 const users = JSON.parse(fs.readFileSync(USERS_FILE))
-const WHITELIST = ['/healthcheck', '/version']
+const AUTHLIST = ['/search']
 
 export default (req, res, next) => {
-  for (const allowed of WHITELIST) {
-    if (req.url === allowed) {
-      return next()
+  for (const authenticated of AUTHLIST) {
+    if (req.path === authenticated) {
+      return basicAuth({
+        users,
+        challenge: true
+      })(req, res, next)
     }
   }
 
-  basicAuth({
-    users,
-    challenge: true
-  })(req, res, next)
+  next();
 }
